606e13c8676d28673a01edb87caf0e3b162d34c9
José Pedro Saraiva <nocive@gmail.com>
2011-10-20 16:02:37 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once( 'simple_file_cache.php' );
-var_dump( SFCache::write( 'foobar', array( 'foo' => 'bat' ), '+30 minutes' ) );
+var_dump( SFCache::write( 'foobar', array( 'foo' => 'bar' ), '+30 minutes' ) );
 var_dump( SFCache::read( 'foobar' ) );
